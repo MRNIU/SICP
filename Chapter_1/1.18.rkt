9e6ef6219805ec8450f27a8a636a5111655ccc10
MRNIU <a6813140@hotmail.com>
2019-01-26 19:08:33 +0800

    update

--- Chapter_1/1.18.rkt
+++ Chapter_1/1.18.rkt
@@ -9,7 +9,7 @@
   (cond
     ((or (= n 0) (= m 0)) curr)
     ((even? n) (product-iter (double m) (halve n) curr))
-    (else (product-iter (+ m curr) (- m 1) (- n 1)))))
+    (else (product-iter m (- n 1) (+ m curr)))))
 
 (define (product m n)
   (product-iter m n 0))
